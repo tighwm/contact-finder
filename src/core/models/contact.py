ae8516ef406f766caed6d67ad622358de051c382
tighwm <partnyor117@gmail.com>
2025-10-16 01:13:47 +0300

    alembic migration for contacts table

--- src/core/models/contact.py
+++ src/core/models/contact.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Index, text
+from sqlalchemy import Index, text, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
@@ -7,10 +7,11 @@ from core.models import Base
 class Contact(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    first_name: Mapped[str] = mapped_column()
-    last_name: Mapped[str] = mapped_column()
-    email: Mapped[str] = mapped_column(unique=True)
-    description: Mapped[str] = mapped_column()
+    nimble_id: Mapped[str] = mapped_column(unique=True, nullable=True)
+    first_name: Mapped[str] = mapped_column(nullable=True)
+    last_name: Mapped[str] = mapped_column(nullable=True)
+    email: Mapped[str] = mapped_column(nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
 
     __table_args__ = (
         Index(
@@ -26,3 +27,9 @@ class Contact(Base):
             postgresql_using="gin",
         ),
     )
+
+    def __str__(self):
+        return f"id={self.id} full name={self.first_name + self.last_name}"
+
+    def __repr__(self):
+        return f"Contact(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, email={self.email}, description={self.description})"
