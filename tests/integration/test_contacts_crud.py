1182b7c825c1c5d226dd85fa9a95ff788392c40a
tighwm <partnyor117@gmail.com>
2025-10-16 21:23:13 +0300

    tests for batch_insert_contacts

--- tests/integration/test_contacts_crud.py
+++ tests/integration/test_contacts_crud.py
@@ -14,3 +14,67 @@ async def test_get_contacts_by_query(session):
     result = await get_contacts_by_query(session, query)
 
     assert result == [ryan]
+
+
+async def test_batch_insert_contacts(session):
+    from api.v1.contact.crud import batch_insert_contacts
+
+    contacts = [
+        {
+            "first_name": "Karl",
+            "last_name": "Marks",
+            "nimble_id": "id",
+        },
+        {
+            "first_name": "Mega",
+            "last_name": "Mind",
+            "nimble_id": "megaid",
+        },
+    ]
+    result = await batch_insert_contacts(
+        session,
+        contacts=contacts,
+    )
+
+    assert len(result) == 2
+
+
+async def test_batch_insert_contacts_with_conflict(session):
+    from sqlalchemy import select
+
+    from api.v1.contact.crud import batch_insert_contacts
+    from core.models import Contact
+
+    sasha = Contact(
+        first_name="sasha",
+        last_name="cover",
+        nimble_id="id",
+    )
+
+    session.add(sasha)
+    await session.flush()
+
+    contacts = [
+        {
+            "first_name": "Karl",
+            "last_name": "Marks",
+            "nimble_id": "id",
+        },
+        {
+            "first_name": "Mega",
+            "last_name": "Mind",
+            "nimble_id": "megaid",
+        },
+    ]
+    result = await batch_insert_contacts(
+        session,
+        contacts=contacts,
+        on_conflict_do_update_index_elements=["nimble_id"],
+    )
+
+    await session.refresh(sasha)
+    stmt = select(Contact)
+    scalar_res = await session.scalars(stmt)
+
+    #  check the len because it will show whether the row was created or updated
+    assert len(scalar_res.all()) == len(result)
